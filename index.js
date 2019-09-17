707190212c3021f88e8877c8765fc1b5df43c79d
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-09-17 21:31:03 +0200

    refactor: enhance variables name

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 "use strict";
 
-function getNextAsyncIteratorValue(iterator, index) {
-    return iterator.next().then((value) => {
-        return { index, value };
+function getNextAsyncIteratorValue(asyncIterator, index) {
+    return asyncIterator.next().then((iterator) => {
+        return { index, iterator };
     });
 }
 
 async function* combineAsyncIterators(...iterators) {
+    // Return if iterators is empty (avoid infinite loop).
+    if (iterators.length === 0) {
+        return;
+    }
+    const promiseThatNeverResolve = new Promise(() => null);
+
     try {
-        if (iterators.length === 0) {
-            return;
-        }
-        const promises = iterators.map(getNextAsyncIteratorValue);
-        let open = iterators.length;
+        const asyncIteratorsValues = iterators.map(getNextAsyncIteratorValue);
+        let numberOfIteratorsAlive = iterators.length;
 
         do {
-            const { value, index } = await Promise.race(promises);
-            if (value.done) {
-                open--;
-                promises[index] = new Promise(() => null);
+            const { iterator, index } = await Promise.race(asyncIteratorsValues);
+            if (iterator.done) {
+                numberOfIteratorsAlive--;
+                // We dont want Promise.race to resolve again on this index
+                // so we replace it with a Promise that will never resolve.
+                asyncIteratorsValues[index] = promiseThatNeverResolve;
             }
             else {
-                yield value.value;
-                promises[index] = getNextAsyncIteratorValue(iterators[index], index);
+                yield iterator.value;
+                asyncIteratorsValues[index] = getNextAsyncIteratorValue(iterators[index], index);
             }
-        } while (open > 0);
+        } while (numberOfIteratorsAlive > 0);
     }
     catch (err) {
+        // TODO: replace .all with .allSettled
         await Promise.all(iterators.map((it) => it.return()));
 
         throw err;
