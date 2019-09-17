524dffd376cfd9bac6ebd7cc2297eb335b069bd6
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-09-17 21:09:32 +0200

    refactor: remove jsdoc

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 "use strict";
 
-/**
- * @typedef {object} AsyncIterableObject
- * @property {number} index
- * @property {IteratorResult<any>} value
- */
-
-/**
- * @function getNextAsyncIteratorValue
- * @param {AsyncIterableIterator<any>} iterator
- * @param {!number} index
- * @returns {Promise<AsyncIterableObject>}
- */
 function getNextAsyncIteratorValue(iterator, index) {
     return iterator.next().then((value) => {
         return { index, value };
     });
 }
 
-/**
- * @async
- * @generator
- * @function combineAsyncIterators
- * @param {...AsyncIterableIterator<any>} iterators
- */
 async function* combineAsyncIterators(...iterators) {
     try {
         if (iterators.length === 0) {
