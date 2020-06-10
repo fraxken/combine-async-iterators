3e2a490f2d13f9593b2aaf3e9dca78df389bb947
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-06-10 15:43:21 -0300

    fixing errorCallback

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,14 @@
 export interface CombineOptions {
     throwError?: boolean;
-    errorCallback: (err: Error) => any;
+    errorCallback?: (err: Error) => any;
 }
 
-declare function combineAsyncIterators(...iterators: AsyncIterableIterator<any>[]): AsyncIterableIterator<any>;
-declare function combineAsyncIterators(options: CombineOptions, ...iterators: AsyncIterableIterator<any>[]): AsyncIterableIterator<any>;
+declare function combineAsyncIterators(
+    ...iterators: AsyncIterableIterator<any>[]
+): AsyncIterableIterator<any>;
+declare function combineAsyncIterators(
+    options: CombineOptions,
+    ...iterators: AsyncIterableIterator<any>[]
+): AsyncIterableIterator<any>;
 
 export = combineAsyncIterators;
-
