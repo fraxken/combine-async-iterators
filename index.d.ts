29810d078e6ff8041f0215fb54372a826c88a1f8
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-06-10 15:35:25 -0300

    Allowing errors to be ignored on one of the iterators

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,10 @@
+export interface CombineOptions {
+    throwError?: boolean;
+    errorCallback: (err: Error) => any;
+}
+
 declare function combineAsyncIterators(...iterators: AsyncIterableIterator<any>[]): AsyncIterableIterator<any>;
+declare function combineAsyncIterators(options: CombineOptions, ...iterators: AsyncIterableIterator<any>[]): AsyncIterableIterator<any>;
 
 export = combineAsyncIterators;
 
--- index.js
+++ index.js
@@ -1,33 +1,68 @@
 "use strict";
 
-function getNextAsyncIteratorValue(asyncIterator, index) {
-    return asyncIterator.next().then((iterator) => {
-        return { index, iterator };
-    });
+function getNextAsyncIteratorFactory(options) {
+    return async(asyncIterator, index) => {
+        try {
+            const iterator = await asyncIterator.next();
+
+            return { index, iterator };
+        }
+        catch (err) {
+            if (options.errorCallback) {
+                options.errorCallback(err);
+            }
+            if (options.throwError !== false) {
+                return Promise.reject(err);
+            }
+
+            return {
+                index,
+                iterator: {
+                    done: true
+                }
+            };
+        }
+    };
 }
 
 async function* combineAsyncIterators(...iterators) {
+    let [options] = iterators;
+
+    if (typeof options.next === "function") {
+        options = {};
+    }
+    else {
+        iterators.shift();
+    }
     // Return if iterators is empty (avoid infinite loop).
     if (iterators.length === 0) {
         return;
     }
     const promiseThatNeverResolve = new Promise(() => null);
+    const getNextAsyncIteratorValue = getNextAsyncIteratorFactory(options);
+
 
     try {
         const asyncIteratorsValues = iterators.map(getNextAsyncIteratorValue);
         let numberOfIteratorsAlive = iterators.length;
 
         do {
-            const { iterator, index } = await Promise.race(asyncIteratorsValues);
-            if (iterator.done) {
-                numberOfIteratorsAlive--;
-                // We dont want Promise.race to resolve again on this index
-                // so we replace it with a Promise that will never resolve.
-                asyncIteratorsValues[index] = promiseThatNeverResolve;
-            }
-            else {
-                yield iterator.value;
-                asyncIteratorsValues[index] = getNextAsyncIteratorValue(iterators[index], index);
+            const response = await Promise.race(
+                asyncIteratorsValues
+            );
+            if (response) {
+                const {
+                    iterator,
+                    index
+                } = response;
+                if (iterator.done) {
+                    numberOfIteratorsAlive--;
+                    asyncIteratorsValues[index] = promiseThatNeverResolve;
+                }
+                else {
+                    yield iterator.value;
+                    asyncIteratorsValues[index] = getNextAsyncIteratorValue(iterators[index], index);
+                }
             }
         } while (numberOfIteratorsAlive > 0);
     }
