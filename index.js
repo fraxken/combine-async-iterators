70dc5e6e84d5767d0b80cd31daa6c4f890ccfd85
Thiago Santos <thiago.santos@maxmilhas.com.br>
2020-06-11 09:30:06 -0300

    undoing map strategy

--- index.js
+++ index.js
@@ -41,33 +41,25 @@ async function* combineAsyncIterators(...iterators) {
     if (iterators.length === 0) {
         return;
     }
+    const promiseThatNeverResolve = new Promise(() => null);
     const getNextAsyncIteratorValue = getNextAsyncIteratorFactory(options);
 
 
     try {
-        const asyncIteratorsValues = new Map();
-        iterators.forEach((it, idx) => asyncIteratorsValues
-            .set(idx, getNextAsyncIteratorValue(it, idx))
-        );
+        const asyncIteratorsValues = iterators.map(getNextAsyncIteratorValue);
         let numberOfIteratorsAlive = iterators.length;
 
         do {
-            const response = await Promise.race(
-                Array.from(asyncIteratorsValues.values())
-            );
-            if (response) {
-                const {
-                    iterator,
-                    index
-                } = response;
-                if (iterator.done) {
-                    numberOfIteratorsAlive--;
-                    asyncIteratorsValues.delete(index);
-                }
-                else {
-                    yield iterator.value;
-                    asyncIteratorsValues.set(index, getNextAsyncIteratorValue(iterators[index], index));
-                }
+            const { iterator, index } = await Promise.race(asyncIteratorsValues);
+            if (iterator.done) {
+                numberOfIteratorsAlive--;
+                // We don't want Promise.race to resolve again on this index
+                // so we replace it with a Promise that will never resolve.
+                asyncIteratorsValues[index] = promiseThatNeverResolve;
+            }
+            else {
+                yield iterator.value;
+                asyncIteratorsValues[index] = getNextAsyncIteratorValue(iterators[index], index);
             }
         } while (numberOfIteratorsAlive > 0);
     }
